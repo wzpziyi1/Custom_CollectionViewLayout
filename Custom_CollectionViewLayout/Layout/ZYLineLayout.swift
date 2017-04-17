90c42c4eb9a0a766e2264e5cdb3cc28b77f9a3bb
wzpziyi1 <1204473090@qq.com>
2017-04-17 20:22:53 +0800

    流水放大布局初步实现

---
+++ Custom_CollectionViewLayout/Layout/ZYLineLayout.swift
@@ -0,0 +1,80 @@
+//
+//  ZYLineLayout.swift
+//  Custom_CollectionViewLayout
+//
+//  Created by 王志盼 on 2017/4/17.
+//  Copyright © 2017年 王志盼. All rights reserved.
+//
+
+import UIKit
+
+class ZYLineLayout: UICollectionViewFlowLayout {
+
+    fileprivate var itemW: CGFloat
+    fileprivate var itemH: CGFloat
+    
+    init(itemW: CGFloat, itemH: CGFloat) {
+        
+        self.itemW = itemW
+        self.itemH = itemH
+        super.init()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    /// 只要显示的边界发生改变就重新布局:
+    /// 内部会重新调用prepareLayout和layoutAttributesForElementsInRect方法获得所有cell的布局属性
+    override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
+        return true
+    }
+    
+    
+    /// 在这个方法里面做初始化的工作
+    override func prepare() {
+        super.prepare()
+        itemSize = CGSize(width: itemW, height: itemH)
+        scrollDirection = .horizontal
+        self.minimumLineSpacing = 70
+        
+        self.sectionInset = UIEdgeInsetsMake(0, collectionView!.bounds.width * 0.5, 0, collectionView!.bounds.width * 0.5)
+    }
+    
+    
+    ///
+    /// - Returns: 所有的cell的attributes属性
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        
+        //当前屏幕可见范围
+        let visibleRect : CGRect = CGRect(origin: collectionView!.contentOffset, size: collectionView!.bounds.size)
+        
+        /// 调用父类方法，拿到所有的attributes属性
+        let oallAttributes = super.layoutAttributesForElements(in: rect)
+        
+        let centerX = collectionView!.contentOffset.x + 0.5 * collectionView!.bounds.width
+        
+        guard let allAttributes = oallAttributes else {
+            return nil
+        }
+        
+        for (_, attribute) in allAttributes.enumerated() {
+            
+            if visibleRect.intersects(attribute.frame) {   //如果当前item在屏幕上
+                let itemCenterX = attribute.center.x;
+                
+                let maxDistance = 0.5 * (collectionView!.bounds.width + itemW)
+                
+                let scale = 1 + (1 - abs(itemCenterX - centerX) / maxDistance) * 0.5
+                
+                attribute.transform = CGAffineTransform(scaleX: scale, y: scale)
+                
+            }
+            
+        }
+        return allAttributes
+    }
+    
+}
--- Custom_CollectionViewLayout/ViewController.swift
+++ Custom_CollectionViewLayout/ViewController.swift
@@ -16,21 +16,21 @@ class ViewController: UIViewController {
     fileprivate lazy var collectionView: UICollectionView = {
         
         let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: 100, height: 100)
-        layout.scrollDirection = .horizontal
         
-        let collectionView = UICollectionView(frame: CGRect(x: 100, y: 100, width: self.view.bounds.width, height: 200), collectionViewLayout: layout)
+        
+        let collectionView = UICollectionView(frame: CGRect(x: 0, y: 100, width: self.view.bounds.width, height: 180), collectionViewLayout: ZYLineLayout(itemW: 100, itemH: 100))
         collectionView.dataSource = self
         collectionView.delegate = self
         
-        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: kReuseIdentifier)
         return collectionView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: kReuseIdentifier)
+        
+        setupUI()
     }
 
     override func didReceiveMemoryWarning() {
@@ -53,11 +53,12 @@ extension ViewController {
 // MARK: - UICollectionViewDataSource, UICollectionViewDelegate
 extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
+        return 30
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: kReuseIdentifier, for: indexPath)
+        cell.backgroundColor = UIColor.red
         return cell
     }
 }
